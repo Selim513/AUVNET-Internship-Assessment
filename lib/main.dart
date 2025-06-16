f4c09b518863799dddcae8ddbabf55a8ac871d86
Ahmed Selim <ahmedshaabanselem@gmail.com>
2025-06-16 19:09:32 +0300

    Update dependencies and register plugins for Supabase and URL launcher support

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:auvnet_flutter_assessment/features/splash_screen/presentation/views/splash_view.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://zkrjbqcgitzdixyfzppp.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprcmpicWNnaXR6ZGl4eWZ6cHBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMzYwMDYsImV4cCI6MjA2NTYxMjAwNn0.V_GBfL8DqyojveNHRuxCjHDeQvKLfaTgNw4XMjem5Z0',
+  );
   runApp(DevicePreview(builder: (context) => MainApp()));
 }
 
